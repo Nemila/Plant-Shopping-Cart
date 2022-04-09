5dda3f8ce096e9ffd74b83c2ab5d690b9452caca
Lamine <pridila.2006@gmail.com>
2022-04-09 00:00:10 -0500

    Warning

--- src/components/PlantItem/index.js
+++ src/components/PlantItem/index.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { motion } from "framer-motion";
-import { MdWaterDrop } from "react-icons/md";
-import { FaCartPlus, FaSun } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
 
 const plantItem = {
   initial: {
@@ -17,29 +16,6 @@ const plantItem = {
 };
 
 export default function PlantItem({ plant, addToCart }) {
-  let careScale = [[], []];
-  useEffect(() => {
-    for (let i = 0; i < plant.sun; i++) {
-      careScale[0].push(<MdWaterDrop />);
-    }
-    for (let i = 0; i < plant.water; i++) {
-      careScale[1].push(<FaSun />);
-    }
-  }, []);
-
-  function handleCare(type) {
-    let careValue = type === "sun" ? careScale[1].length : careScale[0].length;
-    let comment = "";
-    if (careValue < 2) {
-      comment = "peu";
-    } else if (careValue > 2) {
-      comment = "beaucoup";
-    } else {
-      comment = "moyen";
-    }
-    return comment;
-  }
-
   return (
     <motion.div
       className="plantItem"
@@ -52,24 +28,6 @@ export default function PlantItem({ plant, addToCart }) {
         className="plant-cover"
       />
       <h4>{plant.name}</h4>
-      <div className="care-container">
-        <div
-          className="care-box"
-          onClick={() =>
-            alert(`Cette plante demande ${handleCare("water")} d'eau`)
-          }
-        >
-          {careScale[0].map((icon) => icon)}
-        </div>
-        <div
-          className="care-box"
-          onClick={() =>
-            alert(`Cette plante demande ${handleCare("sun")} de soleil`)
-          }
-        >
-          {careScale[1].map((icon) => icon)}
-        </div>
-      </div>
       <motion.button
         whileTap={{ scale: 0.9 }}
         onClick={() => addToCart(plant)}
